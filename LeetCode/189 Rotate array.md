b36bffb2a79d6c6673538fe07f8996f94a4672d1
jimmy1087 <jimmy1087@gmail.com>
2020-05-10 23:41:28 -0500

    Separate MD file definition of problem

---
+++ LeetCode/189 Rotate array.md
@@ -0,0 +1,19 @@
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+
+Runtime: 68 ms  
+Memory: 15.4 MB
+Time Complexity:  O(n)   
+Space complexity: O(n)
+
+---
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+'a[1,2,3,4,5,6]
+
+'a[(i+k)%n] 
+will traverse the array starting from a given K and
+restart index to zero when len of array is reached (MOD %)
+
+---
+
+Solution taken from suggestion in problem.
--- LeetCode/189 Rotate array.py
+++ LeetCode/189 Rotate array.py
@@ -1,20 +1,3 @@
-'''
-https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
-
-Runtime: 68 ms  
-Memory: 15.4 MB
-Time Complexity:  O(n)   
-Space complexity: O(n)
-
-Given an array, rotate the array to the right by k steps, where k is non-negative.
-a[1,2,3,4,5,6]
-
-a[(i+k)%n] 
-will traverse the array starting from a given K and
-restart index to zero when len of array is reached (MOD %)
-
-Solution taken from suggestion in problem.
-'''
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
