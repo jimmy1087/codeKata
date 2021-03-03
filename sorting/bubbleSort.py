155bd24637c90a8329acd1b8ced799a0c95e86bc
jimmy1087 <jimmy1087@gmail.com>
2021-03-02 21:24:08 -0600

    Bubble Sort

--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -1,26 +1,18 @@
-# Easy
-
 def bubbleSort(array):
     sorted = False
-    counter = 0
+    sortedElements = 0
     while not sorted:
         sorted = True
-        for i in range(len(array)-1-counter):
-            if array[i] > array[i + 1]:
-                swap(i, i + 1, array)
+        for i in range(len(array)-sortedElements-1):
+            if array[i] > array[i+1]:
+                swap(i, array)
                 sorted = False
-        counter += 1
+        sortedElements += 1
     return array
 
-def swap(i, j, array):
-    array[i], array[j] = array[j], array[i]
+def swap(i, array):
+    array[i], array[i+1] = array[i+1], array[i]
+        
+a = [3,4,-6,2,1,4,6,-2,-3,0]
 
-import pytest
-
-def test_shortArray():
-    a = [3,2,1]
-    assert [1,2,3] == bubbleSort(a)
-
-def test_longArray():
-    a = [3,4,-6,2,1,4,6,-2,-3,0]
-    assert [-6, -3, -2, 0, 1, 2, 3, 4, 4, 6 ] == bubbleSort(a)
+print(bubbleSort(a))
