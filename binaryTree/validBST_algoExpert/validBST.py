abbcde3041d8fe74b225b8690b4513613e1678c6
jimmy1087 <jimmy1087@gmail.com>
2021-03-28 16:54:00 -0600

    valid BST

--- binaryTree/validBST_algoExpert/validBST.py
+++ binaryTree/validBST_algoExpert/validBST.py
@@ -6,7 +6,9 @@ def validBSTHelper(node, minVal, maxVal):
     if node is None:
         return True
 
-    if node.val < minVal and node.val > maxVal:
+    print(str(minVal)+':'+str(node.val)+':'+str(maxVal))
+
+    if node.val < minVal or node.val > maxVal:
         return False
     
     return validBSTHelper(node.left, minVal, node.val) and validBSTHelper(node.right, node.val, maxVal)
@@ -43,10 +45,9 @@ n5.left, n5.right = n2, n5_2
 n2.left = n1
 n15.left, n15.right = n13, n22
 n13.right = n14
-
+print('TEST VALID BST')
 print(validBST(n10))
-
-
+print('TEST RETURN INVALID BST')
 '''
 Invalid BST
          10
@@ -57,3 +58,33 @@ Invalid BST
    /        \
   1         14
 '''
+
+n10, n5, n15, n2, n4, n1, n13, n22, n14 = N(10), N(5), N(15), N(2), N(4), N(1), N(13), N(22), N(14)
+n10.left, n10.right = n5, n15
+n5.left, n5.right = n2, n4
+n2.left = n1
+n15.left, n15.right = n13, n22
+n13.right = n14
+
+print(validBST(n10))
+
+print('TEST LESS THAN OR EQUAL TO THE RIGHT')
+'''
+Invalid BST
+         5
+       /    \
+      5     15
+     / \   /  \
+    2   5 13  22
+   /        \
+  1         14
+'''
+
+n5_0, n5, n15, n2, n5_2, n1, n13, n22, n14 = N(5), N(5), N(15), N(2), N(5), N(1), N(13), N(22), N(14)
+n5_0.left, n5_0.right = n5, n15
+n5.left, n5.right = n2, n5_2
+n2.left = n1
+n15.left, n15.right = n13, n22
+n13.right = n14
+
+print(validBST(n5_0))
